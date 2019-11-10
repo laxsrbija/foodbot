6d89772d021621e7809625d95ba8f4df4be60358
Lazar Stanojević <laxsrbija@yahoo.com>
2019-11-10 12:13:55 +0100

    Describe the return parameter

--- messaging-module/src/main/java/rs/laxsrbija/foodbot/messaging/service/MessageService.java
+++ messaging-module/src/main/java/rs/laxsrbija/foodbot/messaging/service/MessageService.java
@@ -8,6 +8,8 @@ public interface MessageService
 {
 	/**
 	 * Sends message to a desired group, as defined in the provided {@link Message}.
+	 *
+	 * @return An {@link Optional} containing a {@link LocalDateTime} object with the message server time
 	 * @throws rs.laxsrbija.foodbot.messaging.exception.FoodBotMessagingException when an error occurs
 	 */
 	Optional<LocalDateTime> sendMessage(final Message message);
