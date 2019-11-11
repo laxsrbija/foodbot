abc982b2c8d8c1714821eeaf861faadc693c4d3b
Lazar Stanojević <laxsrbija@yahoo.com>
2019-11-11 19:06:31 +0100

    Add menu parser module

---
+++ foodbot-menu-parser/src/test/java/rs/laxsrbija/foodbot/menuparser/MenuParserModuleApplicationTests.java
@@ -0,0 +1,14 @@
+package rs.laxsrbija.foodbot.menuparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuParserModuleApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
