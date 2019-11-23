7b7a8944979a89d0bd0e2e4b16e0d236bef07848
Lazar Stanojević <laxsrbija@yahoo.com>
2019-11-23 14:27:13 +0100

    Add a Received Menu Item mapper

--- foodbot-common/src/main/java/rs/laxsrbija/foodbot/common/model/mapper/ReceivedMenuItemMapper.java
+++ foodbot-common/src/main/java/rs/laxsrbija/foodbot/common/model/mapper/ReceivedMenuItemMapper.java
@@ -1,6 +1,54 @@
 package rs.laxsrbija.foodbot.common.model.mapper;
 
+import java.time.DayOfWeek;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import rs.laxsrbija.foodbot.common.exception.FoodBotException;
+import rs.laxsrbija.foodbot.common.helper.DateUtils;
+import rs.laxsrbija.foodbot.common.model.dto.ReceivedMenuItemDto;
+import rs.laxsrbija.foodbot.common.model.entity.ReceivedMenuItemEntity;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReceivedMenuItemMapper
 {
+	public static ReceivedMenuItemDto toDto(final ReceivedMenuItemEntity receivedMenuItemEntity)
+	{
+		final ReceivedMenuItemDto.ReceivedMenuItemDtoBuilder dtoBuilder = ReceivedMenuItemDto.builder()
+			.id(receivedMenuItemEntity.getId())
+			.mainCourse(receivedMenuItemEntity.getMainCourse())
+			.salad(receivedMenuItemEntity.getSalad());
 
+		final DayOfWeek dayOfWeek = receivedMenuItemEntity.getDayOfWeek();
+		if (dayOfWeek != null)
+		{
+			dtoBuilder.dayOfWeek(dayOfWeek.getValue());
+		}
+
+		return dtoBuilder.build();
+	}
+
+	public static ReceivedMenuItemEntity fromDto(final ReceivedMenuItemDto receivedMenuItemDto)
+	{
+		final ReceivedMenuItemEntity.ReceivedMenuItemEntityBuilder entityBuilder = ReceivedMenuItemEntity.builder()
+			.id(receivedMenuItemDto.getId())
+			.mainCourse(receivedMenuItemDto.getMainCourse())
+			.salad(receivedMenuItemDto.getSalad());
+
+		final Integer dayOfWeekIndex = receivedMenuItemDto.getDayOfWeek();
+		if (dayOfWeekIndex != null)
+		{
+			try
+			{
+				entityBuilder.dayOfWeek(DateUtils.dayOfWeekFromIndex(dayOfWeekIndex));
+			}
+			catch (final FoodBotException e)
+			{
+				log.warn(e.getMessage());
+			}
+		}
+
+		return entityBuilder.build();
+	}
 }
