302aa27c4898c2f8f4bc8e0cd4d963ee22ed68bb
Lazar Stanojević <laxsrbija@yahoo.com>
2019-11-24 00:12:17 +0100

    Fix Sonar issues

--- foodbot-common/src/main/java/rs/laxsrbija/foodbot/common/model/entity/ConfigurationEntity.java
+++ foodbot-common/src/main/java/rs/laxsrbija/foodbot/common/model/entity/ConfigurationEntity.java
@@ -3,12 +3,14 @@ package rs.laxsrbija.foodbot.common.model.entity;
 import javax.persistence.*;
 import lombok.*;
 
+
 @Data
 @Table
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class ConfigurationEntity extends PlaceholderEntity
 {
 	@Column
