c8bc4842f4bd906a049c8e006bb1a12bcfcafe3f
Lazar Stanojević <laxsrbija@yahoo.com>
2020-01-03 17:30:11 +0100

    Update the Swagger API info

--- foodbot-webapp/src/main/java/rs/laxsrbija/foodbot/webapp/configuration/SwaggerConfiguration.java
+++ foodbot-webapp/src/main/java/rs/laxsrbija/foodbot/webapp/configuration/SwaggerConfiguration.java
@@ -1,9 +1,11 @@
 package rs.laxsrbija.foodbot.webapp.configuration;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,17 @@ public class SwaggerConfiguration
 			.select()
 			.apis(RequestHandlerSelectors.any())
 			.paths(PathSelectors.any())
-			.build();
+			.build()
+			.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+			"FoodBot REST API",
+			"REST endpoints of the FoodBot server.",
+			"1.0.0",
+			"",
+			ApiInfo.DEFAULT_CONTACT,
+			"License", "https://github.com/laxsrbija/foodbot/blob/master/LICENSE", Collections.emptyList());
 	}
 }
