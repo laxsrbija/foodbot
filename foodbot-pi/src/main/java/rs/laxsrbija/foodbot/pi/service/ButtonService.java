e6d20dbde921c8db23c53e7a8b629b6b3d440ecd
Lazar Stanojević <laxsrbija@yahoo.com>
2020-01-03 16:06:42 +0100

    Send a random food arrival message when the button is pressed

--- foodbot-pi/src/main/java/rs/laxsrbija/foodbot/pi/service/ButtonService.java
+++ foodbot-pi/src/main/java/rs/laxsrbija/foodbot/pi/service/ButtonService.java
@@ -1,18 +1,51 @@
 package rs.laxsrbija.foodbot.pi.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import rs.laxsrbija.foodbot.common.model.entity.ArrivalEntity;
+import rs.laxsrbija.foodbot.common.service.ArrivalService;
+import rs.laxsrbija.foodbot.messaging.exception.FoodBotMessagingException;
+import rs.laxsrbija.foodbot.messaging.service.MessageService;
 import rs.laxsrbija.foodbot.pi.model.BuzzType;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ButtonService
 {
 	private final BuzzerService _buzzerService;
+	private final ArrivalService _arrivalService;
+	private final MessageService _messageService;
 
 	public void buttonAction()
 	{
-		// TODO: Once the food arrival logic is in place, it should be interacted with from here
-		_buzzerService.buzz(BuzzType.SUCCESS);
+		try
+		{
+			sendFoodArrivalMessage();
+			_buzzerService.buzz(BuzzType.SUCCESS);
+		}
+		catch (final Exception e)
+		{
+			log.error("Unable to send the food arrival notification: " + e.getMessage());
+		}
+	}
+
+	private void sendFoodArrivalMessage()
+	{
+		final ArrivalEntity randomFoodArrivalMessage = _arrivalService.random();
+		final String arrivalMessage = randomFoodArrivalMessage.getArrivalMessage();
+
+		final Optional<LocalDateTime> dateTimeReceived = _messageService.sendMessage(arrivalMessage);
+
+		if (!dateTimeReceived.isPresent())
+		{
+			throw new FoodBotMessagingException("Time of message reception is not present");
+		}
+
+		final LocalDateTime localDateTime = dateTimeReceived.get();
+		log.info("Food arrival message received at " + localDateTime);
 	}
 }
