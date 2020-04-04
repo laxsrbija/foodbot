54a697e4ed62480900d7edc3d81bd4b43877e95e
Lazar Stanojević <lazars@niri-ic.com>
2020-04-04 23:53:51 +0200

    Add a today's menu component

--- foodbot-frontend/src/app/app.module.ts
+++ foodbot-frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { HomeComponent } from './core/home/home.component';
 import { InstantMessageComponent } from './core/home/instant-message/instant-message.component';
+import { TodaysMenuComponent } from './core/home/todays-menu/todays-menu.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
 		HomeComponent,
-		InstantMessageComponent
+		InstantMessageComponent,
+		TodaysMenuComponent
 	],
 	imports: [
 		BrowserModule
---
+++ foodbot-frontend/src/app/core/home/todays-menu/todays-menu.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-todays-menu',
+	templateUrl: './todays-menu.component.html',
+	styleUrls: ['./todays-menu.component.scss']
+})
+export class TodaysMenuComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+}
