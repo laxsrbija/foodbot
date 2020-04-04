b9e64dbae64e39863c1c6039911f15a45f177b7c
Lazar Stanojević <lazars@niri-ic.com>
2020-04-04 19:12:07 +0200

    Add layout to the instant messaging component

--- foodbot-frontend/src/app/app.module.ts
+++ foodbot-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { HomeComponent } from './core/home/home.component';
+import { InstantMessageComponent } from './core/home/instant-message/instant-message.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		NavbarComponent,
+		HomeComponent,
+		InstantMessageComponent
 	],
 	imports: [
 		BrowserModule
---
+++ foodbot-frontend/src/app/core/home/instant-message/instant-message.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-instant-message',
+	templateUrl: './instant-message.component.html',
+	styleUrls: ['./instant-message.component.css']
+})
+export class InstantMessageComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
